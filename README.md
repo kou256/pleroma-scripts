75117023e2d057fcfd2d93330aa54d8d2f7a6648
kou256 <25428510+kou256@users.noreply.github.com>
2023-12-17 21:57:43 +0900

    Update: バックアップ用スクリプトの修正
    名称の変更及び各種ファイルの保存場所を変更した．

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 ### rclone.conf
 - `/root/.config/rclone/rclone.conf`として配置する
 - Cloudflare R2のアクセスキーID、シークレットアクセスキー、エンドポイントをファイル内に書き込む
-### backup.sh
-- `/root/backup.sh`として配置する（仮）
+### db_backup.sh
+- `/var/lib/sbin/db_backup.sh`として配置する
 - PostgreSQLのダンプファイルをgzipで圧縮する
 - 圧縮したファイルをrcloneでR2バケットにアップロードする
-### backup_schedule
-- `/etc/cron.d/backup_schedule`として配置する
-- 毎日3:00(JST)にbackup.shを実行してバックアップしたファイルをアップロードする
+### db_backup_schedule
+- `/etc/cron.d/db_backup_schedule`として配置する
+- 毎日3:00(JST)にdb_backup.shを実行してバックアップしたファイルをアップロードする
 - 毎日3:10(JST)に14日以上経過したバックアップファイルを削除する
--- db_backup/db_backup.sh
+++ db_backup/db_backup.sh
@@ -2,18 +2,19 @@
 
 TIME=$(date +%Y%m%d_%H-%M-%S)
 BACKUP_FILE="${TIME}-db-backup.gz"
+BACKUP_DIR="/var/lib/postgresql/backup"
 
 # Stop Pleroma Service
 systemctl stop pleroma
 
 # Create DB Backup
 echo "Create DB backup"
-su postgres -s $SHELL -lc "pg_dumpall | gzip -c > /tmp/backup/${BACKUP_FILE}"
+su postgres -s $SHELL -lc "pg_dumpall | gzip -c > ${BACKUP_DIR}/${BACKUP_FILE}"
 echo "Create DB backup finished."
 
 # Upload backup to Cloudflare R2
 echo "Upload backup file"
-cd /tmp/backup
+cd ${BACKUP_DIR}
 rclone copy ${BACKUP_FILE} r2:/backup/
 echo "Upload finished"
 
